d5b6c5c6a3b8218a5ee4e41eeb6472e40604cc19
ikekou0015 <ikekou0015@gmail.com>
2020-11-02 20:23:04 +0900

    ユーザー新規登録、ログイン機能実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,15 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :nickname,   presence: true
+  validates :birth_date, presence: true
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: '全角文字を使用してください' } do
+  validates :last_name
+  validates :first_name
+  validates :last_name_kana
+  validates :first_name_kana
+  end
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  validates_format_of :password, with: PASSWORD_REGEX, message: 'には英字と数字の両方を含めて設定してください' 
 end
--- db/migrate/20201102031428_devise_create_users.rb
+++ db/migrate/20201102031428_devise_create_users.rb
@@ -6,8 +6,12 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.0]
       ## Database authenticatable
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
-      t.nick_name
-      t.
+      t.string :nickname,          null: false, default: ""
+      t.date :birth_date
+      t.string :last_name
+      t.string :first_name
+      t.string :last_name_kana
+      t.string :first_name_kana
 
       ## Recoverable
       t.string   :reset_password_token
