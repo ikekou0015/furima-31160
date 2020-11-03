a60c58c09cc17ea111197ff0cbb8c662f907572b
ikekou0015 <ikekou0015@gmail.com>
2020-11-03 17:29:20 +0900

    テストコードの実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,14 +4,17 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :nickname,   presence: true
+  validates :nickname,   presence: true, length: { maximum: 40 }
   validates :birth_date, presence: true
+
   with_options presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: '全角文字を使用してください' } do
   validates :last_name
   validates :first_name
+end
+  with_options presence: true, format: { with: /\A[ァ-ン一ー]+\z/, message: 'はカタカナで入力して下さい'} do
   validates :last_name_kana
-  validates :first_name_kana
-  end
-  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  validates :first_name_kana  
+end
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze 
   validates_format_of :password, with: PASSWORD_REGEX, message: 'には英字と数字の両方を含めて設定してください' 
 end
---
+++ spec/factories/users.rb
@@ -0,0 +1,13 @@
+FactoryBot.define do
+  factory :user do
+    nickname              {Faker::Name.initials(number: 2)}
+    email                 {Faker::Internet.free_email}
+    password              {Faker::Internet.password(min_length: 6)}
+    password_confirmation {password}
+    last_name             {"ああああ"}
+    first_name            {"ああああ"}
+    last_name_kana        {"アアアア"}
+    first_name_kana       {"アアアア"}
+    birth_date            {"2000-01-01"}
+  end
+end
