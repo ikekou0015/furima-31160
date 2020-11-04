94702ef09786774964f7e18914f45a281c76d7c9
ikekou0015 <ikekou0015@gmail.com>
2020-11-04 21:12:19 +0900

    出品登録

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -6,12 +6,21 @@ class ItemsController < ApplicationController
 
   def new
     @item = Item.new
-     #@genre = Genre.where(genre)
   end
 
   def create
+    @item = Item.new(item_params)
+     if @item.save
+        redirect_to root_path
+     else
+      render :new
+     end
   end
 
   def destroy
+  end
+
+  def item_params
+    params.require(:item). permit(:name, :info, :price, :category_id, :sales_status, :shipping_fee_status, :prefecture, :scheduled_delivery)
   end
 end
