cbb8871be111dad5570a46053acbfae8d42c7689
ikekou0015 <ikekou0015@gmail.com>
2020-11-06 22:36:34 +0900

    削除機能追加

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :edit]
   before_action :move_to_index, only: [:edit]
-  before_action :set_tweet, only: [:show, :edit, :update]
+  before_action :set_tweet, only: [:show, :edit, :update, :destroy]
 
   def index
     @items = Item.order('created_at DESC')
@@ -35,6 +35,7 @@ class ItemsController < ApplicationController
   end
 
   def destroy
+    @item.destroy
   end
  
   private
