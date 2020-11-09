be1ea7d54bef0ee2e591300ee551375aab86ef23
ikekou0015 <ikekou0015@gmail.com>
2020-11-09 18:08:14 +0900

    購入機能テストコード

---
+++ spec/factories/form.rb
@@ -0,0 +1,9 @@
+FactoryBot.define do
+  factory :form do
+    postal_code          {'111-0000'}
+    prefecture_id        {'2'}
+    city                 {'あああ'}
+    addresses            {'あああ'}
+    phone_number         {'0000000000'}
+  end
+end
