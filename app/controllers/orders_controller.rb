58038b7126839b803ed54cb6de928b28841d2752
ikekou0015 <ikekou0015@gmail.com>
2020-11-10 09:41:42 +0900

    購入ページへの制限

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,4 +1,7 @@
 class OrdersController < ApplicationController
+  before_action :authenticate_user!, only: [:index]
+  before_action :move_to_index, only: [:index]
+
   def index
     @item = Item.find(params[:item_id])
     @form = Form.new
@@ -22,4 +25,10 @@ end
    def form_params
     params.require(:form).permit(:postal_code, :prefecture_id, :city, :addresses, :building, :phone_number ).merge(item_id: params[:item_id], user_id: current_user.id)
    end
+
+   def move_to_index
+    if current_user.id == Item.find(params[:item_id]).user_id
+      redirect_to root_path
+    end
+  end
 end
