54e9bc04f9c1af895c4ecfae534866734ea30be7
ikekou0015 <ikekou0015@gmail.com>
2020-11-11 11:06:09 +0900

    購入ページの制限

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,7 +1,8 @@
 class OrdersController < ApplicationController
   before_action :authenticate_user!, only: [:index]
-  before_action :move_to_index, only: [:index]
   before_action :set_item, only: [:index, :create]
+  before_action :move_to_index, only: [:index]
+  
   def index
     @form = Form.new
   end
@@ -33,7 +34,7 @@ class OrdersController < ApplicationController
     )
   end
   def move_to_index
-    if current_user.id == Item.find(params[:item_id]).user_id
+    if current_user.id == Item.find(params[:item_id]).user_id || @item.order != nil 
         redirect_to root_path
     end
   end
